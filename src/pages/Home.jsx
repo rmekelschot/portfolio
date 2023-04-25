934f16ab5159b70254fb9b277d1af217d7f9d70d
rmekelschot <rm.ekelschot@gmail.com>
2023-04-25 14:27:22 +0200

    Grid system

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,25 @@
 import * as React from "react";
 import Header from "../components/Header";
-import { Box, Container, Typography } from "@mui/material";
+import { Box, Container, Divider, Grid, Typography } from "@mui/material";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBootstrap, faCss3, faDocker, faGit, faGithub, faHtml5, faJava, faJs, faLinkedin, faLinux, faNode, faPhp, faPython, faReact, faUnity, faVuejs } from "@fortawesome/free-brands-svg-icons";
+import {
+  faBootstrap,
+  faCss3,
+  faDocker,
+  faGit,
+  faGithub,
+  faHtml5,
+  faJava,
+  faJs,
+  faLinkedin,
+  faLinux,
+  faNode,
+  faPhp,
+  faPython,
+  faReact,
+  faUnity,
+  faVuejs,
+} from "@fortawesome/free-brands-svg-icons";
 import { Link } from "react-router-dom";
 
 function Home() {
@@ -14,63 +31,90 @@ function Home() {
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
-          height: "90vh",
         }}
       >
-        <Box sx={{ height: "50vh" }}>
-          <Box sx={{ marginBottom: "100px", display: 'flex', justifyContent: 'center'}}>
-            <Box sx={{ marginRight: '30px' }}>
-              <Typography variant="h3" gutterBottom sx={{ fontWeight: "bold" }}>
-                Front-end developer 👋
-              </Typography>
-              <Typography variant="subtitle1" className="text-light">
-                Hoi, ik ben Mitchell Ekelschot.
-              </Typography>
-              <Typography
-                variant="subtitle1"
-                className="text-light"
-                gutterBottom
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={4}>
+            <Typography variant="h4" gutterBottom sx={{ fontWeight: "bold" }}>
+              Front-end developer 👋
+            </Typography>
+            <Typography variant="subtitle1" className="text-light">
+              Hoi, ik ben Mitchell Ekelschot.
+            </Typography>
+            <Typography variant="subtitle1" className="text-light" gutterBottom>
+              Een front-end developer uit Zuid-Holland, Nederland. 📍
+            </Typography>
+            <Link
+              className="text text-dark"
+              to="https://www.linkedin.com/in/mitchell-ekelschot-204248271/"
+              target="_blank"
+            >
+              <FontAwesomeIcon className="icon" icon={faLinkedin} />
+            </Link>
+            <Link
+              className="text text-dark"
+              to="https://github.com/rmekelschot"
+              target="_blank"
+            >
+              <FontAwesomeIcon className="icon" icon={faGithub} />
+            </Link>
+          </Grid>
+          <Grid item xs={12} md={8}>
+            <Box sx={{display: 'flex', justifyContent: 'center'}}>
+              <Box
+                sx={{
+                  width: "250px",
+                  height: "250px",
+                  backgroundColor: "#000",
+                }}
               >
-                Een front-end developer uit Zuid-Holland, Nederland. 📍
-              </Typography>
-
-              <Link
-                className="text text-dark"
-                to="https://www.linkedin.com/in/mitchell-ekelschot-204248271/"
-                target="_blank"
-              >
-                <FontAwesomeIcon className="icon" icon={faLinkedin} />
-              </Link>
-              <Link
-                className="text text-dark"
-                to="https://github.com/rmekelschot"
-                target="_blank"
-              >
-                <FontAwesomeIcon className="icon" icon={faGithub} />
-              </Link>
+                Image
+              </Box>
             </Box>
-            <Box sx={{width: '250px', height: '250px', backgroundColor: '#000'}}>
-              Image
+          </Grid>
+          <Grid item xs={12}>
+            <Typography variant="h6" gutterBottom>
+              Vaardigheden:{" "}
+            </Typography>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
+              <Typography>
+                <FontAwesomeIcon className="icon" icon={faReact} />
+                <FontAwesomeIcon className="icon" icon={faVuejs} />
+                <FontAwesomeIcon className="icon" icon={faJs} />
+                <FontAwesomeIcon className="icon" icon={faHtml5} />
+                <FontAwesomeIcon className="icon" icon={faCss3} />
+                <FontAwesomeIcon className="icon" icon={faBootstrap} />
+              </Typography>
+              <Divider
+                orientation="vertical"
+                flexItem
+                sx={{ margin: "0px 10px" }}
+              />
+              <Typography>
+                <FontAwesomeIcon className="icon" icon={faPhp} />
+                <FontAwesomeIcon className="icon" icon={faNode} />
+                <FontAwesomeIcon className="icon" icon={faDocker} />
+              </Typography>
+              <Divider
+                orientation="vertical"
+                flexItem
+                sx={{ margin: "0px 10px" }}
+              />
+              <Typography>
+                <FontAwesomeIcon className="icon" icon={faLinux} />
+                <FontAwesomeIcon className="icon" icon={faPython} />
+                <FontAwesomeIcon className="icon" icon={faJava} />
+                <FontAwesomeIcon className="icon" icon={faUnity} />
+                <FontAwesomeIcon className="icon" icon={faGit} />
+              </Typography>
             </Box>
-          </Box>
-          <Box sx={{ display: "flex", alignItems: "center" }}>
-            <Typography variant="h6">Vaardigheden: </Typography>
-            <FontAwesomeIcon className="icon" icon={faReact} />
-            <FontAwesomeIcon className="icon" icon={faVuejs} />
-            <FontAwesomeIcon className="icon" icon={faJs} />
-            <FontAwesomeIcon className="icon" icon={faHtml5} />
-            <FontAwesomeIcon className="icon" icon={faCss3} />
-            <FontAwesomeIcon className="icon" icon={faBootstrap} />
-            <FontAwesomeIcon className="icon" icon={faPhp} />
-            <FontAwesomeIcon className="icon" icon={faLinux} />
-            <FontAwesomeIcon className="icon" icon={faJava} />
-            <FontAwesomeIcon className="icon" icon={faPython} />
-            <FontAwesomeIcon className="icon" icon={faNode} />
-            <FontAwesomeIcon className="icon" icon={faUnity} />
-            <FontAwesomeIcon className="icon" icon={faDocker} />
-            <FontAwesomeIcon className="icon" icon={faGit} />
-          </Box>
-        </Box>
+          </Grid>
+        </Grid>
       </Container>
     </div>
   );
