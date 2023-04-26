ed537a0cd7e79f785933c70471e59287b62579a3
rmekelschot <rm.ekelschot@gmail.com>
2023-04-26 22:13:43 +0200

    tooltips

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import Header from "../components/Header";
-import { Box, Container, Divider, Grid, Typography } from "@mui/material";
+import { Box, Container, Divider, Grid, Tooltip, Typography } from "@mui/material";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faBootstrap,
@@ -43,10 +43,10 @@ function Home() {
             >
               Front-End Developer 👋
             </Typography>
-            <Typography variant="subtitle1" className="text-light">
+            <Typography variant="subtitle2" className="text-light" sx={{fontSize: '18px'}}>
               Hoi, ik ben Mitchell Ekelschot.
             </Typography>
-            <Typography variant="subtitle1" className="text-light" gutterBottom>
+            <Typography variant="subtitle2" className="text-light" sx={{fontSize: '18px'}} gutterBottom>
               Een front-end developer uit Zuid-Holland, Nederland. 📍
             </Typography>
             <Link
@@ -86,12 +86,26 @@ function Home() {
               }}
             >
               <Typography>
+
+              <Tooltip title="React">
                 <FontAwesomeIcon className="icon" icon={faReact} />
+              </Tooltip>
+              <Tooltip title="VueJs">
                 <FontAwesomeIcon className="icon" icon={faVuejs} />
+              </Tooltip>
+              <Tooltip title="Javascript">
                 <FontAwesomeIcon className="icon" icon={faJs} />
+              </Tooltip>
+              <Tooltip title="HTML">
                 <FontAwesomeIcon className="icon" icon={faHtml5} />
+              </Tooltip>
+              <Tooltip title="Css">
                 <FontAwesomeIcon className="icon" icon={faCss3} />
+              </Tooltip>
+              <Tooltip title="Bootstrap">
                 <FontAwesomeIcon className="icon" icon={faBootstrap} />
+              </Tooltip>
+
               </Typography>
               <Divider
                 orientation="vertical"
@@ -99,21 +113,38 @@ function Home() {
                 sx={{ margin: "0px 10px" }}
               />
               <Typography>
+
+              <Tooltip title="PHP">
                 <FontAwesomeIcon className="icon" icon={faPhp} />
+              </Tooltip>
+              <Tooltip title="NodeJs">
                 <FontAwesomeIcon className="icon" icon={faNode} />
+              </Tooltip>
+              <Tooltip title="Docker">
                 <FontAwesomeIcon className="icon" icon={faDocker} />
+              </Tooltip>
               </Typography>
               <Divider
                 orientation="vertical"
                 flexItem
                 sx={{ margin: "0px 10px" }}
-              />
+                />
               <Typography>
+              <Tooltip title="Linux">
                 <FontAwesomeIcon className="icon" icon={faLinux} />
+                </Tooltip>
+              <Tooltip title="Python">
                 <FontAwesomeIcon className="icon" icon={faPython} />
+                </Tooltip>
+              <Tooltip title="Java">
                 <FontAwesomeIcon className="icon" icon={faJava} />
+                </Tooltip>
+              <Tooltip title="Unity3D">
                 <FontAwesomeIcon className="icon" icon={faUnity} />
+                </Tooltip>
+              <Tooltip title="Git">
                 <FontAwesomeIcon className="icon" icon={faGit} />
+                </Tooltip>
               </Typography>
             </Box>
           </Grid>
