5e67d1dd6607a30c48599c0856141b9a653575e2
rmekelschot <rm.ekelschot@gmail.com>
2023-05-07 18:39:19 +0200

    navbar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './nav.css'
+import {AiOutlineHome, AiOutlineUser} from 'react-icons/ai'
+import {BiBook, BiMessageSquareDetail} from 'react-icons/bi'
+import {RiServiceLine} from 'react-icons/ri'
 
 const Nav = () => {
+
+  const [activeNav, setActiveNav] = useState('#');
+
   return (
-    <div>Nav</div>
+    <nav>
+      <a className={activeNav === '#' ? 'active' : ''} onClick={() => setActiveNav('#')} href="/#"><AiOutlineHome /></a>
+      <a className={activeNav === '#about' ? 'active' : ''} onClick={() => setActiveNav('#about')} href="/#about"><AiOutlineUser /></a>
+      <a className={activeNav === '#experience' ? 'active' : ''} onClick={() => setActiveNav('#experience')} href="/#experience"><BiBook /></a>
+      <a className={activeNav === '#services' ? 'active' : ''} onClick={() => setActiveNav('#services')} href="/#services"><RiServiceLine /></a>
+      <a className={activeNav === '#contact' ? 'active' : ''} onClick={() => setActiveNav('#contact')} href="/#contact"><BiMessageSquareDetail /></a>
+    </nav>
   )
 }
 
